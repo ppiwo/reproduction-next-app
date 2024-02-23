ade9eef665c09b9fa3705babd70a44a14a0a863d
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2024-02-23 16:59:34 -0600

    Add repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import { headers } from "next/headers";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+
+  // Run `next build`
+  try {
+    headers();
+  } catch (err) {
+    console.error("CAUGHT: ", err);
+  }
+
+  return <div>page</div>;
 }
